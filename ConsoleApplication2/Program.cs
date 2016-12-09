ee5a70c98dd09abd77b4d9e32c22bb22fdc3ff0b
Tom Spenger <htl-spenger-2013@gmx.at>
2016-12-09 13:45:44 +0100

    weitere zeile

--- ConsoleApplication2/Program.cs
+++ ConsoleApplication2/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApplication2
         static void Main(string[] args)
         {
             Console.WriteLine("hallo");
-            Console.WriteLine("hallo");
+            Console.WriteLine("welt");
             Console.WriteLine("hallo");
         }
     }
