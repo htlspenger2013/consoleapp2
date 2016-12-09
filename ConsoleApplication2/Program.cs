0b43c906edd38e6d3c2c00bb64fcbeda423826e6
Tom Spenger <htl-spenger-2013@gmx.at>
2016-12-09 13:36:01 +0100

    erster checkin

--- ConsoleApplication2/Program.cs
+++ ConsoleApplication2/Program.cs
@@ -11,6 +11,8 @@ namespace ConsoleApplication2
         static void Main(string[] args)
         {
             Console.WriteLine("hallo");
+            Console.WriteLine("hallo");
+            Console.WriteLine("hallo");
         }
     }
 }
